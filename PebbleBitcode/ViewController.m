3ffef61c22939cc20afa7078232619a5212d82ed
Nathan Corvino <nathan@corvino.com>
2016-10-24 10:56:24 -0700

    Initial single view Xcode 8 template.

---
+++ PebbleBitcode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PebbleBitcode
+//
+//  Created by Nathan Corvino on 10/24/16.
+//  Copyright © 2016 Nathan Corvino. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
